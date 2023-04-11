bd3e31ff765577723716975844012b6e428d438e
abil samedov <abil.samedov2016@yandex.ru>
2023-04-11 21:03:53 +0400

    ch11

--- books/models.py
+++ books/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 
@@ -19,4 +20,12 @@ class Book(models.Model):
     class Meta:
         verbose_name = 'Книгу'
         verbose_name_plural = 'Книги'
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     
+    def __str__(self) -> str:
+        return self.review
--- books/tests.py
+++ books/tests.py
@@ -1,15 +1,17 @@
 from http import HTTPStatus
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Book
+from .models import Book, Review
 
 
 class BookTests(TestCase):
     def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(username='reviewuser', email='reviewuser@gmail.com', password='admin12345')
         self.book = Book.objects.create(title='Harry Potter', author='name', price='25.00',)
-
+        self.review = Review.objects.create(book = self.book, author=self.user, review = 'Nice!',)
 
     def test_book_listing(self):
         self.assertEqual(f'{self.book.title}', 'Harry Potter')
@@ -28,5 +30,6 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(no_response.status_code, HTTPStatus.NOT_FOUND)
         self.assertContains(response, 'Harry Potter')
+        self.assertContains(response, 'Nice!')
         self.assertTemplateUsed(response, 'books/book_detail.html')
  
